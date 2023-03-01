014f3a00f24cc80c950f99954552e59b4992db9a
ramiro <102091809+ramiroogk@users.noreply.github.com>
2023-03-01 16:25:18 -0300

    Issues 1,2,3

--- keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
+++ keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
@@ -1,17 +1,35 @@
 package com.identicum.keycloak;
 
+import java.util.Random;
+import org.jboss.logging.Logger;
+import static org.jboss.logging.Logger.getLogger;
+
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.keycloak.authentication.AuthenticationFlowContext;
-import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
+import org.keycloak.authentication.authenticators.browser.PasswordForm;
 import org.keycloak.forms.login.LoginFormsProvider;
+		
+public class CustomAuthenticator extends PasswordForm {
 
-public class CustomAuthenticator extends UsernamePasswordForm {
-
+	private static final Logger logger = getLogger(CustomAuthenticator.class);
+	String token;
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
 		// Generar codigo random y mostrarlo en el theme
 		LoginFormsProvider form = context.form();
+	
+		Random random = new Random();
+		token = random.ints(48, 122 + 1)
+        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+        .limit(10)
+        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+        .toString();
+		
+
+		logger.infov("TOKEN GENERADO! {0}", token);
+		form.setAttribute("token", token);
+
 
 		super.authenticate(context);
 	}
@@ -21,7 +39,17 @@ public class CustomAuthenticator extends UsernamePasswordForm {
 		// Obtener informacion de la request y validar si es igual al codigo generado
 		MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
 		
-		super.action(context);
+		String pass = formData.getFirst("password");
+		logger.infov("password in action {0}", pass);
+
+		if(!pass.equals(token)){
+			logger.infov("not equals {0}", pass);
+			return;
+		}
+		
+		context.success();
+
+		// super.action(context);
 	}
 
 }
